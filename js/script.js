034f579e002473113e893c39edd0f2e6243a8bb1
Satsukevich Dzmitry <dimamoving@gmail.com>
2021-03-22 01:36:24 +0300

    add nav-dots for slider

--- js/script.js
+++ js/script.js
@@ -289,6 +289,7 @@ window.addEventListener('DOMContentLoaded', function () {
 //переменные
 const slider = document.querySelector('.offer__slider'),
     wrapperWidth = window.getComputedStyle(slider.querySelector('.offer__slider-wrapper')).width,
+    wrapper = slider.querySelector('.offer__slider-wrapper'),
     inner = slider.querySelector('.offer__slider-inner'),
     nextBtn = slider.querySelector('.offer__slider-next'),
     currentInd = slider.querySelector('#current'),
@@ -318,11 +319,15 @@ then(data => {
     });
 }).
 then(() => {
-    const images = slider.querySelectorAll('.offer__slide');
+    const images = slider.querySelectorAll('.offer__slide'),
+        dots = [];
     inner.style.cssText = `width:${100 * images.length}%; display: flex; transition: 0.5s all;`;
     images.forEach(image => image.style.width = wrapperWidth);
-
-
+    const sliderDots = document.createElement('ol');
+    sliderDots.classList.add('carousel-indicators');
+    wrapper.append(sliderDots);
+    placeDots(images, sliderDots, dots);
+    dotChange(images, dots, 'data-order');
     nextBtn.addEventListener('click', () => {
         if (offset == +wrapperWidth.slice(0, wrapperWidth.length - 2) * (images.length - 1)) {
             offset = 0;
@@ -330,8 +335,8 @@ then(() => {
             offset += +wrapperWidth.slice(0, wrapperWidth.length - 2);
         }
         inner.style.transform = `translateX(-${offset}px)`;
-        changeSlideIndex("next", images);
-        showSlideInd(images);
+        changeSlideIndex("next", images, dots, "data-order");
+        showSlideInd(images, dots);
     });
 
 
@@ -342,15 +347,15 @@ then(() => {
             offset -= +wrapperWidth.slice(0, wrapperWidth.length - 2);
         }
         inner.style.transform = `translateX(-${offset}px)`;
-        changeSlideIndex("prev", images);
-        showSlideInd(images);
+        changeSlideIndex("prev", images, dots, 'data-order');
+        showSlideInd(images, dots);
     });
 });
 // Функция изменения индекса слайда
 function changeSlideIndex(dir, arr) {
     if (dir == 'next') {
         ++count;
-    } else {
+    } else if (dir == 'prev') {
         --count;
     }
     if (count > arr.length) {
@@ -359,8 +364,24 @@ function changeSlideIndex(dir, arr) {
         count = arr.length;
     }
 }
+// Функция изменения индекса слайда по точкам
+function dotChange(arr, dotArr, atribute) {
+    dotArr.forEach(a => {
+        a.addEventListener('click', (e) => {
+            const targetInd = e.target.getAttribute(atribute);
+            count = targetInd;
+            offset = +wrapperWidth.slice(0, wrapperWidth.length - 2) * (targetInd - 1);
+            inner.style.transform = `translateX(-${offset}px)`;
+            dotArr.forEach(dot => {
+                dot.style.opacity = '.5';
+            });
+            dotArr[count - 1].style.opacity = 1;
+            showSlideInd(arr, dotArr);
+        });
+    });
+}
 // Функция отображения нового индекса
-function showSlideInd(arr) {
+function showSlideInd(arr, dotArr) {
     if (arr.length < 10) {
         totalInd.textContent = `0${arr.length}`;
         currentInd.textContent = `0${count}`;
@@ -368,7 +389,24 @@ function showSlideInd(arr) {
         totalInd.textContent = arr.length;
         currentInd.textContent = count;
     }
+    dotArr.forEach(dot => {
+        dot.style.opacity = '.5';
+    });
+    dotArr[count - 1].style.opacity = 1;
 
+}
+//Функция отрисовывания точек
+function placeDots(arr, parent, pushArr) {
+    for (let i = 0; i < arr.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-order', i + 1);
+        dot.classList.add('dot');
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+        parent.append(dot);
+        pushArr.push(dot);
+    }
 }
 // Класс картинки на слайдере
 class SliderImage {
